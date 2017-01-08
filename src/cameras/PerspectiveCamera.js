f4e33d4ed3289c32049f24f20fec01ed2e2f3d4f
stannnn <shitaoguo@gmail.com>
2017-01-08 14:34:13 -0800

    add PerspectiveCamera

---
+++ src/cameras/PerspectiveCamera.js
@@ -0,0 +1,19 @@
+import {
+  PerspectiveCamera as PerspectiveCameraNative
+} from 'three'
+
+import { Camera } from './Camera'
+
+export class PerspectiveCamera extends Camera {
+  constructor (config) {
+    super(config)
+    this._obj = new PerspectiveCameraNative(
+      config.camera.fov,
+      config.camera.aspect,
+      config.camera.near,
+      config.camera.far
+    )
+    this.init()
+  }
+}
+
